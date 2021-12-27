c7b93c53d4ca35f010e1f53b492bb0dd772a3b38
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-12-27 05:36:43 +0000

    parsing http headers and added commits

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const http = require('http')
 const url = require('url')
 
+
+//create server to handle all requests
 const server = http.createServer(function(req, res){
     
+    //get and parse the url
     const parsedUrl = url.parse( req.url, true) 
 
+    //get the path
     const path = parsedUrl.pathname
 
+    //remove extraneous slashes
     const trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
+    //get the string as an object
+    const queryStringObject = parsedUrl.query
+
+    //get the request method
     const method = req.method.toLowerCase()
 
+    //get request headers
+    const headers = req.headers
+
+    //send the response
     res.end('Hello World\n')
 
-    console.log('Request recieved on path '+ trimmedPath + 'with this method ' + method)
+    console.log('Request recieved with these headers: ', headers)
 
 })
 
