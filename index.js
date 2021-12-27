fe676436a6bb61fe06316b6d9bf46ec900874237
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-12-27 05:20:05 +0000

    parsing urls

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer(function(req, res){
+    
+    const parsedUrl = url.parse( req.url, true) 
+
+    const path = parsedUrl.pathname
+
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
     res.end('Hello World\n')
+
+    console.log('Request recieved on path '+ trimmedPath)
+
 })
 
 
